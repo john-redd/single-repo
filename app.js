51443f993890bbc6a5b0370865685c65214e7e6c
john-redd <redd.web.development@gmail.com>
2020-09-15 11:37:35 -0600

    Basic app

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+app.use(express.static(__dirname + '/client/build'))
+
+app.get('/api', (req, res) => res.status(200).send('API ready'))
+
+app.get('*', (req, res) =>
+  res.sendFile(require('path').join(__dirname, '/client/build/index.html'))
+)
+
+app.listen(3000, () => console.log('Server running'))
